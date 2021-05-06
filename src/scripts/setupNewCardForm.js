684307d78d3f8966098d53030e50551b9946fe5c
Morten Möller <git@mortenm.de>
2021-05-06 23:44:50 +0200

    Add submitevent and push data to array

--- src/scripts/setupNewCardForm.js
+++ src/scripts/setupNewCardForm.js
@@ -1,4 +1,6 @@
-export default function setupNewCardForm() {
+import renderQuizCard from './renderQuizCard'
+
+export default function setupNewCardForm(cards) {
   const form = document.createElement('form')
   form.setAttribute('data-card-form')
   form.setAttribute('class', 'card-form')
@@ -14,6 +16,23 @@ export default function setupNewCardForm() {
     <label for="tags>Tags</label>
         <input type="text" name="tags" required/>
     <button data-button>Create new card</button>
-    
     `
+  document.body.main.innerHTML = ''
+  document.body.main.append(form)
+  setupFormButton(form, cards)
+}
+
+function setupFormButton(form, array) {
+  form.addEventlistener('submit', event => {
+    event.preventDefault()
+    const { question, answer, tags } = form.elements
+
+    const card = {
+      question: question.value,
+      answer: answer.value,
+      tags: tags.value.split(','),
+    }
+    array.push(card)
+    renderQuizCard(card)
+  })
 }
